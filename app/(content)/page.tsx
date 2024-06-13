9764f3c0bedc0b79537191467cb179faee2e2013
JDaniloC <jdsc@cin.ufpe.br>
2024-06-13 14:58:03 -0300

    Add page title

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -4,7 +4,10 @@ import VideoSlide from "./components/videos";
 export default async function IndexPage() {
 
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-center flex-col'>
+      <h1 className='white font-bold text-6xl m-4 mb-10'>
+        Narrativas AI
+      </h1>
       <div className="max-w-4xl">
         <VideoSlide />
       </div>
